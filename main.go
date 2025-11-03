6bfb257363337d79e0be95426d7770572c325281
maksimwest00 <maksim.west00@gmail.com>
2025-11-03 19:29:14 +0300

    fix

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"backend-go/application"
 	_ "backend-go/docs"
 
 	"github.com/gorilla/mux"
@@ -31,6 +32,25 @@ func init() {
 func handleSwagger() {
 	// Swagger UI будет доступен по http://localhost:8080/swagger/index.html
 	http.Handle("/swagger/", httpSwagger.WrapHandler)
+}
+
+// @Tags Продукты
+// @Summary Создать продукт
+// @Description Создать продукт
+// @Accept json
+// @Produce json
+// @Param JSON body api.CreateProductRequestDto true "CreateProductRequestDto"
+// @Success 200 {object} string
+// @Router /products [POST]
+func CreateProductHanlder(
+	w http.ResponseWriter,
+	r *http.Request) {
+
+	err := application.CreateProductHanlder(w, r)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func main() {
